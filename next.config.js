001db440b89b4190cd08fa48d9100f0a3e407a42
Tim Chen <lilichen396@gmail.com>
2025-04-08 13:53:01 +0800

    fix: update build configuration and dependencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
+module.exports = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
 };
-
-export default nextConfig;
